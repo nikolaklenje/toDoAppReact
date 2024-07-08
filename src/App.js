9b2c4949a8992adc84930f66044085385caf35de
nikolaklenje <nikolaelite88@gmail.com>
2024-07-08 15:11:06 -0400

    To do list basic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [listItem, setListItem] = useState();
+  const submitToList = (e) => {
+    e.preventDefault();
+    if (listItem) {
+      setToDoList([...toDoList, listItem]);
+      setListItem("");
+    }
+  };
   return (
     <main className="App">
       <div className="container">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          onChange={(e) => {
+            setListItem(e.target.value);
+          }}
+          type="text"
+        ></input>
+        <button onClick={submitToList}>Add to the list</button>
+        <div>
+          <p>TO DO List:</p>
+          <ul>
+            {toDoList.map((todo) => (
+              <li key={todo}>{todo}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
 }
 
 export default App;
+
+//Delete i done
+
+//Edit
+//dodavanje na drugoj strani
