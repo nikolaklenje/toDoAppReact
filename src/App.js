b0f730aa46bc65e3fdfa377a9ed68d57f7a04f79
nikolaklenje <nikolaelite88@gmail.com>
2024-07-08 19:24:40 -0400

    separate adding items

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [toDoList, setToDoList] = useState([]);
   const [listItem, setListItem] = useState();
   const [completeItems, setCompleteItemsList] = useState([]);
+  const [addingItems, setAddingItems] = useState(false);
   const submitToList = (e) => {
     e.preventDefault();
     if (listItem) {
       setToDoList([...toDoList, listItem]);
+      setAddingItems(false);
       setListItem("");
     }
   };
@@ -22,47 +24,56 @@ function App() {
     doneList.push(index);
     setCompleteItemsList(doneList);
   };
+
   return (
     <main className="App">
       <div className="container">
-        <input
-          onChange={(e) => {
-            setListItem(e.target.value);
-          }}
-          type="text"
-          value={listItem}
-        ></input>
-        <button onClick={submitToList}>Add to the list</button>
-        <div>
-          <p>TO DO List:</p>
-          <ul>
-            {toDoList.map((todo, index) => (
-              <li
-                className={
-                  completeItems.includes(index)
-                    ? "list-item-complete"
-                    : "list-item"
-                }
-                key={index}
-              >
-                {todo}
-                <p
-                  className="list-item-option"
-                  onClick={() => handleComplete(index)}
+        {addingItems ? (
+          <div>
+            <input
+              onChange={(e) => {
+                setListItem(e.target.value);
+              }}
+              type="text"
+              value={listItem}
+            ></input>
+            <button onClick={submitToList}>Add Item</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={() => setAddingItems(true)}>
+              Add Items To a List?
+            </button>
+            <p>TO DO List:</p>
+            <ul>
+              {toDoList.map((todo, index) => (
+                <li
+                  className={
+                    completeItems.includes(index)
+                      ? "list-item-complete"
+                      : "list-item"
+                  }
+                  key={index}
                 >
-                  complete
-                </p>
-                <p
-                  className="list-item-option"
-                  onClick={() => handleDelete(index)}
-                >
-                  delete
-                </p>
-                <p className="list-item-option">edit</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+                  {todo}
+                  <p
+                    className="list-item-option"
+                    onClick={() => handleComplete(index)}
+                  >
+                    complete
+                  </p>
+                  <p
+                    className="list-item-option"
+                    onClick={() => handleDelete(index)}
+                  >
+                    delete
+                  </p>
+                  <p className="list-item-option">edit</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </main>
   );
